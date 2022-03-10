8b655affbb957c78bdda41ac6e763825f133f015
naimxno <naimxno@gmail.com>
2022-03-10 21:50:58 +0600

    user picture fixed

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
   const image = post.image;
+  console.log(post);
   const div = document.createElement("article");
   div.classList.add("post");
   div.innerHTML = `
@@ -62,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -161,6 +162,7 @@ const loadPosts = async () => {
   let data = await fetch('../data/posts.json');
   posts = await data.json();
   showPosts(posts);
+  // console.log(posts);
 }
 
 loadPosts();
